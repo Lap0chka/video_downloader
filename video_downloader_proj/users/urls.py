7c6b0c7e386668ce3a52c8d9ae8e2fa53e344e91
Daniil <danya.tkachenko.1997@gmail.com>
2024-06-10 11:24:48 +0200

    add app downloader

--- video_downloader_proj/users/urls.py
+++ video_downloader_proj/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 app_name = 'user'
 
 urlpatterns = [
     path('login/', views.MyLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- video_downloader_proj/video_downloader_proj/settings.py
+++ video_downloader_proj/video_downloader_proj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'downloader',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -127,3 +129,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #AUTH
 
 AUTH_USER_MODEL = 'users.User'
+LOGOUT_REDIRECT_URL = 'user:login'
